8430379e73d0d8ff08b9b48de37f5f890298d6e3
TheArtist <agorglouk@gmail.com>
2015-09-10 18:17:50 +0300

    Implemented Navigation Drawer

--- src/main/java/com/scaryboxstudios/sampleapp/MainActivity.java
+++ src/main/java/com/scaryboxstudios/sampleapp/MainActivity.java
@@ -2,15 +2,90 @@ package com.scaryboxstudios.sampleapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.NavigationView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+    /**/
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Show the hamburger
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.accessibility_open_drawer,
+                R.string.accessibility_close_drawer
+        );
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(new NavigationDrawerItemSelectedListener());
+
+        // Begin fragment transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.content_frame, new SampleFragment());
+        // Complete the changes added above
+        ft.commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle
+        if (mDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handles clicks on the navigation menu.
+     */
+    private class NavigationDrawerItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(final MenuItem menuItem) {
+            // Update highlighted item in the navigation menu
+            //menuItem.setChecked(true);
+            int itemId = menuItem.getItemId();
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+
+            Bundle args = new Bundle();
+            args.putString("tc", menuItem.getTitle().toString());
+            Fragment ft = new SampleFragment();
+            ft.setArguments(args);
+
+            getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, ft)
+                .commit();
+            return true;
+        }
     }
 }
---
+++ src/main/java/com/scaryboxstudios/sampleapp/SampleFragment.java
@@ -0,0 +1,29 @@
+package com.scaryboxstudios.sampleapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+public class SampleFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sample_fragment, container, false);
+
+        // Set the text contents
+        Bundle args = getArguments();
+        if (args != null) {
+            String textContents = args.getString("tc");
+            if (textContents != null) {
+                TextView tv = (TextView) view.findViewById(R.id.text_contents);
+                tv.setText(textContents);
+            }
+        }
+
+        return view;
+    }
+}
